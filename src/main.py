60ac3c49946b43b744bf277e1742bf564a688fa2
jiaojiaodubai <jiaojiaodubai23@qq.com>
2022-11-28 16:01:10 +0800

    去掉src中的部分ignore规则

--- src/main.py
+++ src/main.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     g = guide.Guide()
 
 # TODO：找到mac创建文件夹的快捷方式（替身）的API，替换Win的Dispatch("WScript.Shell")
-# TODO：在mac上测试不同的问价复制方法，看是否有进度提示，如果没有，设置进度等待界面。
+# TODO：在mac上测试不同的文件复制方法，看是否有进度提示，如果没有，设置进度等待界面。
